c131204c574b2ec0671fc5912f27f382c913e734
dockz9 <zach@stoneparkllc.com>
2026-03-31 08:38:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Win This Moment!',
-        short_name: 'Win This Moment',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#080810',
-        theme_color: '#080810',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      }
-    })
   ],
 })
